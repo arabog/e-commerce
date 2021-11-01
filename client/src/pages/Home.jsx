1c05610da782969f00c39243cc5045a04e9c13e8
iCode_Rx <arababs2015@gmail.com>
2021-11-01 22:17:39 +0100

    footer added

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,6 +4,7 @@ import Slider from "../components/Slider"
 import Categories from "../components/Categories"
 import Products from "../components/Products"
 import Newsletter from "../components/Newsletter"
+import Footer from "../components/Footer"
 
 
 const Home = () => {
@@ -12,6 +13,7 @@ const Home = () => {
           return (
                     <div>
                               <Announcement />
+                              
                               <Navbar />
 
                               <Slider />
@@ -21,6 +23,8 @@ const Home = () => {
                               <Products />
 
                               <Newsletter />
+
+                              <Footer />
                     </div>
           )
 }
