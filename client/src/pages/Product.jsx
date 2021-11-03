c4e6da0932522e0af205524ffc0d465e7b0cd914
iCode_Rx <arababs2015@gmail.com>
2021-11-04 00:33:19 +0100

    Product page modified

--- client/src/pages/Product.jsx
+++ client/src/pages/Product.jsx
@@ -8,41 +8,121 @@ import Newsletter from "../components/Newsletter"
 import Footer from "../components/Footer"
 
 
-const Container = styled.div ``
+const Container = styled.div `
+          
+`
 
-const Wrapper= styled.div ``
+const Wrapper= styled.div `
+          padding: 50px;
+          display: flex;
+`
 
-const ImgContainer = styled.div ``
+const ImgContainer = styled.div `
+          flex: 1;
+`
 
-const Image = styled.div ``
+const Image = styled.img `
+          width: 100%;
+          height: 90vh;
+          object-fit: cover;
+`
 
-const InfoContainer = styled.div ``
+const InfoContainer = styled.div `
+          flex: 1;
+          padding: 0 50px;
+`
 
-const Title = styled.h1 ``
+const Title = styled.h1 `
+          font-weight: 200;
+`
 
-const Desc = styled.div ``
+const Desc = styled.p `
+          margin: 20px 0;
+`
 
-const Price = styled.div ``
+const Price = styled.span `
+          font-weight: 100;
+          font-size: 40px;
+`
 
-const FilterContainer = styled.div ``
+const FilterContainer = styled.div `
+          width: 50%;
 
-const Filter = styled.div ``
+          margin: 30px 0;
 
-const FilterTitle = styled.h1 ``
+          display: flex;
+          justify-content: space-between;
+`
 
-const FilterColor = styled.h1 ``
+const Filter = styled.div `
+          display: flex;
+          align-items: center;
+`
 
-const FilterSize = styled.div ``
+const FilterTitle = styled.span `
+          font-size: 20px;
+          font-weight: 200;
+`
 
-const FilterSizeOption = styled.div ``
+const FilterColor = styled.div `
+          width: 20px;
+          height: 20px;
 
-const AddContainer = styled.div ``
+          border-radius: 50%;
+          background: ${props => props.color}
 
-const AmountContainer = styled.div ``
+          margin: 0 50px;
+          cursor: pointer;
+`
 
-const Amount = styled.h1 ``
+const FilterSize = styled.select `
+          margin-left: 10px;
+          padding: 5px;
+`
 
-const Button = styled.button ``
+const FilterSizeOption = styled.div `
+
+`
+
+const AddContainer = styled.div `
+          width: 50%;
+          display: flex;
+          align-items: center;
+          justify-content: space-between;
+`
+
+const AmountContainer = styled.div `
+          display: flex;
+          align-item: center;
+          font-weight: 700;
+`
+
+const Amount = styled.span `
+          width: 30px;
+          height: 30px;
+
+          border-radius: 10px;
+          border: 1px solid teal;
+
+          display: flex;
+          align-items: center;
+          justify-content: center;
+
+          margin: 0 5px;
+`
+
+const Button = styled.button `
+          padding: 15px;
+          border: 2px solid teal;
+          background: white;
+          cursor: pointer;
+
+          font-weight: 500;
+
+          &:hover {
+                    background: #f8f4f4;
+          }
+`
 
 
 
@@ -66,7 +146,7 @@ const Product = () => {
 
                                                   <Desc> Product Desc</Desc>
 
-                                                  <Price> $200.00</Price>
+                                                  <Price> $200</Price>
 
                                                   <FilterContainer>
                                                             <Filter>
@@ -88,7 +168,7 @@ const Product = () => {
                                                             <AmountContainer>
                                                                       <Remove />
 
-                                                                      <Amount> Quantity </Amount>
+                                                                      <Amount> 4 </Amount>
 
                                                                       <Add />
                                                             </AmountContainer>
@@ -104,5 +184,6 @@ const Product = () => {
                     </Container>
           )
 }
+
 
 export default Product
